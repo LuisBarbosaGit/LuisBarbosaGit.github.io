09c302105484a7cb5a754ff9b4a1b2981337926e
LuisbarbosaGit <luisbarbosa.santos@outlook.com>
2025-05-16 10:29:45 -0300

    Alterações css

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const title = document.getElementById('title');
-
 const observer = new IntersectionObserver((items, observer) => {
-       items.forEach(item => {
-      if (item.isIntersecting) {
+  items.forEach(item => {
+    if (item.isIntersecting) {
+      setTimeout(() => {
         item.target.classList.add('show');
         observer.unobserve(item.target); // para parar de observar após aparecer
-      }
-    });
-  },  {
-    threshold: 0.2, // quando 20% do elemento estiver visível
+      }, 200);
+    }
   });
+},  {
+  threshold: 0.8, // quando 20% do elemento estiver visível
+});
 
-observer.observe(title)
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.getElementById('title');
+    observer.observe(title)
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const containt = document.querySelectorAll('#tec-cards');
+      showItem(containt)
+})
+
+//Função para enviar cada um dos cards com um intervalo de tempo entre si
+function showItem(container) {
+  let i = 1
+  container.forEach((item =>{
+    setTimeout(() => {
+      observer.observe(item)
+      
+    }, i  * 1800);
+    i++
+    }))   
+  }
+
